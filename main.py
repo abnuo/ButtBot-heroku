cfe174a1bfcce6fa108d4565eeece927e6cf50e8
abnuo <66645994+abnuo@users.noreply.github.com>
2021-05-07 20:19:11 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, abort, Response, redirect, jsonify, send_from_directory
+import os
+import logging
+app = Flask(__name__, static_folder='/')
+logging.basicConfig(level=logging.INFO)
+#hi
+
+@app.route('/')
+def index():
+    return "Hi"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
