8cdc9c1ba888f16f56ce8149ecebd9a9dcc32d9a
abnuo <66645994+abnuo@users.noreply.github.com>
2021-05-07 20:29:53 +0100

    Update server.py

--- server.py
+++ server.py
@@ -9,6 +9,5 @@ logging.basicConfig(level=logging.INFO)
 def index():
     return "Hi"
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
